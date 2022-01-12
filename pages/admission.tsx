63ce7febf6284f2905f5146ee038b75f0ac23968
Mahmoud AlAsad Workstation <mahmoud.alasad@csuglobal.edu>
2022-01-12 12:11:02 +0400

    Update admission.tsx

--- pages/admission.tsx
+++ pages/admission.tsx
@@ -29,7 +29,7 @@ export default function AdmissionPage({otherapps,majordata,hod,staff}) {
                     <a className="nav-link active" href="#applytoscd" data-bs-toggle="tab">Apply to SCD</a>
                   </li>
                   <li className="nav-item">
-                    <a className="nav-link" href="/program/major/28" data-bs-toggle="tab">General Foundation Program</a>
+                    <a className="nav-link" href="/program/major/28">General Foundation Program</a>
                   </li>
                   <li className="nav-item">
                     <a className="nav-link" href="#admissionfees" data-bs-toggle="tab">Admission Fees</a>
