e9242319a0f6c05cf036c94dbf6b29d2ff7fa705
Mahmoud AlAsad Workstation <mahmoud.alasad@csuglobal.edu>
2024-03-24 12:26:44 +0400

    Update index.tsx

--- components/section-home-events/index.tsx
+++ components/section-home-events/index.tsx
@@ -5,7 +5,7 @@ export default function SectionHomeEvents({events,dates}) {
     <section className="event">
         <div className="container">
             <div className="row">
-                <div className="col-lg-6" style={{backgroundColor:"#ffffff"}}>
+                <div className="col-lg-12" style={{backgroundColor:"#ffffff"}}>
                 <div className="fb-page" data-href="https://www.facebook.com/SCD.Art.Design/" data-tabs="timeline, events" data-width="1320" data-height="600" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/SCD.Art.Design/" className="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/SCD.Art.Design/">‎الكلية العلمية للتصميم- The Scientific College of Design‎</a></blockquote></div>
                 </div>
             </div>
