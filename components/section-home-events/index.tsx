c7ab7dc4189e73fa4803ca095dfad6f87e8524b6
Mahmoud AlAsad Workstation <mahmoud.alasad@csuglobal.edu>
2024-03-24 12:21:53 +0400

    aa

--- components/section-home-events/index.tsx
+++ components/section-home-events/index.tsx
@@ -6,7 +6,7 @@ export default function SectionHomeEvents({events,dates}) {
         <div className="container">
             <div className="row">
                 <div className="col-lg-6" style={{backgroundColor:"#ffffff"}}>
-                <div class="fb-page" data-href="https://www.facebook.com/SCD.Art.Design/" data-tabs="timeline, events" data-width="1320" data-height="600" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/SCD.Art.Design/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/SCD.Art.Design/">‎الكلية العلمية للتصميم- The Scientific College of Design‎</a></blockquote></div>
+                <div className="fb-page" data-href="https://www.facebook.com/SCD.Art.Design/" data-tabs="timeline, events" data-width="1320" data-height="600" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/SCD.Art.Design/" className="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/SCD.Art.Design/">‎الكلية العلمية للتصميم- The Scientific College of Design‎</a></blockquote></div>
                 </div>
             </div>
         </div>
