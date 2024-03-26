17ceef46d5e03b177dd005e303a4109a9333807e
Mahmoud AlAsad Workstation <mahmoud.alasad@csuglobal.edu>
2024-03-26 10:15:59 +0400

    Update index.tsx

--- components/section-news-events/newsevents/index.tsx
+++ components/section-news-events/newsevents/index.tsx
@@ -10,10 +10,6 @@ function NewsEvents({ data }) {
             <div className="fb-page" data-href="https://www.facebook.com/SCD.Art.Design/" data-tabs="timeline, events" data-width="1320" data-height="600" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/SCD.Art.Design/" className="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/SCD.Art.Design/">‎الكلية العلمية للتصميم- The Scientific College of Design‎</a></blockquote></div>
           </div>
           <div className="single-blog-grid-item-01 col-lg-6 item">
-          <script src="https://static.elfsight.com/platform/platform.js" data-use-service-core defer></script>
-          <div className="elfsight-app-82f6b637-5b8d-49ad-b733-8c8e68a20eb4" data-elfsight-app-lazy></div>
-          </div>
-          <div className="single-blog-grid-item-01 col-lg-6 item">
           <a className="twitter-timeline" data-width="500" data-height="600" href="https://twitter.com/scd_oman?ref_src=twsrc%5Etfw">Tweets by scd_oman</a> 
           </div>
       </div>      
