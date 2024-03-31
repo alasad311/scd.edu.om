fdaf8af0cd703acd6bd454b38f9985e8bec2a4da
Mahmoud AlAsad Workstation <mahmoud.alasad@csuglobal.edu>
2024-03-31 10:34:31 +0400

    Update index.tsx

--- components/alumni-president/president/index.tsx
+++ components/alumni-president/president/index.tsx
@@ -11,7 +11,7 @@ function Preisdent({ data }) {
         <div className="col-lg-8">
           <div className="testimonial testimonial-style-2 testimonial-with-quotes mb-0">
             <blockquote>
-              <p className="mb-0 internal-content-text" >Distinguished guests,<br /><br />
+              <p className="mb-0 internal-content-text" >Distinguished guests,<br />
 Our role as graduates of the Scientific College of Design is not limited to our academic achievements but extends to our active contribution to the realization of Oman Vision 2040.<br />
 This ambitious vision foresees a prosperous future for the Sultanate, built on innovation, economic diversification, and sustainable development.<br />
 As members of the Alumni Club, we stand today on the threshold of a new era, committed to being part of this national vision, contributing our ideas and projects that reflect our authentic Omani identity and aspirations towards the future.
