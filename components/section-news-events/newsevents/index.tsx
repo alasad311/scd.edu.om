baabcaeee6039c6bd74663bdbaedbde515de3b55
Mahmoud AlAsad Workstation <mahmoud.alasad@csuglobal.edu>
2024-04-02 12:19:29 +0400

    aa

--- components/section-news-events/newsevents/index.tsx
+++ components/section-news-events/newsevents/index.tsx
@@ -7,7 +7,7 @@ function NewsEvents({ data }) {
     return (
       <div className="blog-carousel-01 row"> 
           <div className="single-blog-grid-item-01 col-lg-6 item">
-          <div className='sk-instagram-feed' data-embed-id='25391503'></div>
+            <div data-mc-src="a6a86b7e-8423-4d41-8bcc-9882ffbca2ab#instagram"></div>
           </div>
           <div className="single-blog-grid-item-01 col-lg-6 item">
           <a className="twitter-timeline" data-width="500" data-height="600" href="https://twitter.com/scd_oman?ref_src=twsrc%5Etfw">Tweets by scd_oman</a> 
